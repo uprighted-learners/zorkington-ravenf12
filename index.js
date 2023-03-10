a9e6c9c5099a7e24f65d156ad15932aa61ba6a01
Raven Fisher <ravenfisher1122@gmail.com>
2023-03-09 19:18:39 -0700

    Half Finished Project

--- index.js
+++ index.js
@@ -10,11 +10,22 @@ function ask(questionText) {
 start();
 
 async function start() {
-  const welcomeMessage = `182 Main St.
-You are standing on Main Street between Church and South Winooski.
-There is a door here. A keypad sits on the handle.
-On the door is a handwritten sign.`;
-  let answer = await ask(welcomeMessage);
-  console.log('Now write your code to make this work!');
-  process.exit();
+  console.log(`Game Started`);
+let answer = await ask(`Welcome to Dog, the new shelter adventure. Use the code "Comet" to enter and see all the rescues!`);
+
+if ("Comet"){
+  return console.log(`You enter a room with 5 dogs on the left, and 5 on the right. All of these are small dogs. There is a door to the North with a big red button that says "DO NOT PUSH". Above it is a window that is slightly cracked. In the corner is a ladder, some dogFood, and tennisBall`);
+} else if (state) {
+  return "I don't understand"
 }
+
+  console.log(answer);
+  // process.exit();
+}
+
+  if ("Pick up dogFood", "Get ladder", "Get tennisBall", "Get dogFood", "Pick up ladder", "Pick up tennisBall"){
+  return "TAKEN";
+  } else if (state) {
+    return "I don't understand"
+  }
+
